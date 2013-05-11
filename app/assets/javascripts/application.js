645e3a2f7c15afe15edff1b483f75c2139c1e3a0
Paul Reilly <preilly@teladoc.com>
2013-05-11 16:38:14 -0400

    updates to support better viewso fo sectors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,54 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+  // this variable must be defined this way
+  var YAHOO = {
+    Finance: {
+      SymbolSuggest: {}
+    }
+  };
+
 $(document).ready(function() {
 	$("#search_button").click(function(){
 		window.location.href = "/security/" + $('#search_symbol').val();
 	});
+
+  $('#search_symbol').typeahead({
+    source: function (request, process) {
+      $.ajax({
+        url: "http://autoc.finance.yahoo.com/autoc",
+        type: 'get',
+        dataType: 'jsonp',
+        jsonp: "callback",
+        jsonpCallback: "YAHOO.Finance.SymbolSuggest.ssCallback",
+        data: {
+            query: request
+        },
+        success: function (jsonResult) {
+            console.log("in success");
+            console.log(jsonResult);
+            return typeof jsonResult == 'undefined' ? false : process(jsonResult);
+        }
+      });
+      YAHOO.Finance.SymbolSuggest.ssCallback = function (jsonResult) {
+        tickers = [];
+        //console.log(JSON.stringify(jsonResult));
+        //console.log(JSON.stringify(jsonResult.ResultSet.Result));
+        $.each(jsonResult.ResultSet.Result, function() {
+            //console.log(this.name);
+            tickers.push(this.symbol+" - "+this.name);
+        });
+            return typeof jsonResult == 'undefined' ? false : process(tickers);
+      }
+    },
+
+    updater: function (item) {
+      //selectedState = map[item].stateCode;
+      console.log(item);
+      console.log(item.substring(0,item.indexOf(" -")));
+      return item.substring(0,item.indexOf(" -"));
+    }
+  });
 
 	$("#search_symbol").keypress(function (e) {
 	  if (e.which == 13) {
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
   def home
 	@allSecurityViews = SecurityView.select("security_id, count(security_id) as views").includes(:security).group("security_id").order("views desc")
 	@allSectorViews = SecurityView.select("company_sectors.name, company_sectors.id, count(company_sectors.id) as views").joins(:security, :company_sector).group("company_sectors.id").order("views desc")
+  @allIndustryViews = SecurityView.select("company_industries.name, company_industries.id, count(company_industries.id) as views").joins(:security, :company_industry).group("company_industries.id").order("views desc")
   end
 
   def about
