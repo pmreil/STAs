b8c2160806b0b31cd9d27f2c3fcc34ff2ac87b7e
Paul Reilly <preilly@teladoc.com>
2013-07-21 12:42:21 -0400

    more pg fixes

--- app/controllers/company_sector_controller.rb
+++ app/controllers/company_sector_controller.rb
@@ -1,7 +1,7 @@
 class CompanySectorController < ApplicationController
 
   def index
-    @allSectorViews = SecurityView.select("company_sectors.name as name, count(security_views.id)").joins(:company_sector,:security).includes(:security,:company_sector).group("securities.company_sector_id,security_views.id").order("count(security_id) desc").limit(10)
+    @allSectorViews = SecurityView.select("company_sectors.name as name, count(security_views.id)").joins(:company_sector,:security).includes(:security,:company_sector).group("securities.company_sector_id").order("count(security_id) desc").limit(10)
     #@todaysSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",1,Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
     #@thisWeeksSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",1,Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
   end
@@ -9,13 +9,14 @@ class CompanySectorController < ApplicationController
 
   def show
     if params[:id].nil?
-      #they didnt pass anything in - abort
       @errors = 'Sector Required'
     else
       @sector = CompanySector.where(:id => (params[:id])).first
-      @allSectorViews = SecurityView.where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id,security_views.id").order("count(security_id) desc").limit(10)
-      @todaysSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
-      @thisWeeksSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
+      #@allSectorViews = SecurityView.select("securities.name as security_name,securities.ticker as security_ticker").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
+      #@allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",1).includes(:security).group("security_id").order("count(security_id) desc").limit(10)
+      @allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc")
+      @todaysSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
+      @thisWeeksSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
       if @sector.nil?
         @errors = 'Sector not found'
       end
