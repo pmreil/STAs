0ed805b9238069dce8124f06876c3f61ebaab009
Paul Reilly <preilly@teladoc.com>
2013-07-20 21:08:08 -0400

    changes to how th epostgre setup works

--- app/controllers/company_industry_controller.rb
+++ app/controllers/company_industry_controller.rb
@@ -1,17 +1,22 @@
 class CompanyIndustryController < ApplicationController
 
+  def index
+    @allIndustryViews = SecurityView.select("company_industry.name as name, count(security_views.id)").joins(:company_industry).joins(:security).includes(:security,:company_industry).group("securities.company_industry_id").order("count(security_id) desc").limit(10)
+  end
+
+
   def show
     if params[:id].nil?
       #they didnt pass anything in - abort
       @errors = 'Industry Required'
     else
       @industry = CompanyIndustry.where(:id => (params[:id])).first
-      if @industry.nil?
-        @errors = 'Industry not found'
-      else
-        #@topfive = Security.
+      @allIndustryViews = SecurityView.where("securities.company_industry_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
+      @todaysIndustryViews = SecurityView.where("securities.company_industry_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
+      @thisWeeksIndustryViews = SecurityView.where("securities.company_industry_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
+      if @sector.nil?
+        @errors = 'Sector not found'
       end
     end
   end
-
 end
--- app/controllers/company_sector_controller.rb
+++ app/controllers/company_sector_controller.rb
@@ -1,7 +1,7 @@
 class CompanySectorController < ApplicationController
 
   def index
-    @allSectorViews = SecurityView.select("company_sectors.name as name, count(security_views.id)").joins(:company_sector).joins(:security).includes(:security,:company_sector).group("securities.company_sector_id").order("count(security_id) desc").limit(10)
+    @allSectorViews = SecurityView.select("company_sectors.name as name, count(security_views.id)").joins(:company_sector,:security).includes(:security,:company_sector).group("securities.company_sector_id,securities.security_id").order("count(security_id) desc").limit(10)
     #@todaysSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",1,Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
     #@thisWeeksSectorViews = SecurityView.where("securities.company_sector_id = ? and security_views.created_at >= ?",1,Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
   end
