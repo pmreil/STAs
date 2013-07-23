a14ae5884a79e4d89cdff7134f12ef586c8e69a9
Paul Reilly <preilly@teladoc.com>
2013-07-23 15:51:03 -0400

    added fund category pages

--- app/controllers/company_sector_controller.rb
+++ app/controllers/company_sector_controller.rb
@@ -14,7 +14,7 @@ class CompanySectorController < ApplicationController
       @sector = CompanySector.where(:id => (params[:id])).first
       #@allSectorViews = SecurityView.select("securities.name as security_name,securities.ticker as security_ticker").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
       #@allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",1).includes(:security).group("security_id").order("count(security_id) desc").limit(10)
-      @allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc")
+      @allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
       @todaysSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc")
       @thisWeeksSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc")
       if @sector.nil?
