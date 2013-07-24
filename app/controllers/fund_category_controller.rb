76918ccd7692c734ef1f457b1b196e107db0678c
Paul Reilly <preilly@teladoc.com>
2013-07-23 21:44:57 -0400

    more pg fixes

--- app/controllers/fund_category_controller.rb
+++ app/controllers/fund_category_controller.rb
@@ -7,8 +7,8 @@ class FundCategoryController < ApplicationController
     else
       @category = FundCategory.where(:id => (params[:id])).first
       @allCategoryViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_category_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
-      @todaysCategoryViews = SecurityView.where("securities.fund_category_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
-      @thisWeeksCategoryViews = SecurityView.where("securities.fund_category_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
+      @todaysCategoryViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_category_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
+      @thisWeeksCategoryViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_category_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
       if @family.nil?
         @errors = 'Fund category not found'
       end
--- app/controllers/fund_family_controller.rb
+++ app/controllers/fund_family_controller.rb
@@ -9,8 +9,8 @@ class FundFamilyController < ApplicationController
 
       #@allSectorViews = SecurityView.select("security_id,count(security_id)").where("securities.company_sector_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc")
       @allFamilyViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_family_id = ?",params[:id]).joins(:security).group("security_id").order("count(security_id) desc").limit(10)
-      @todaysFamilyViews = SecurityView.where("securities.fund_family_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
-      @thisWeeksFamilyViews = SecurityView.where("securities.fund_family_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
+      @todaysFamilyViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_family_id = ? and security_views.created_at >= ?",params[:id],Time.now-1.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
+      @thisWeeksFamilyViews = SecurityView.select("security_id,count(security_id)").where("securities.fund_family_id = ? and security_views.created_at >= ?",params[:id],Time.now-7.days).joins(:security).group(:security_id).order("count(security_id) desc").limit(10)
       if @family.nil?
         @errors = 'Fund family not found'
       end
