1da283247df0435077a9eb72706d3f7d0ab323db
Paul Reilly <preilly@teladoc.com>
2013-07-27 16:48:33 -0400

    changes to homepage mapy

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,7 @@ class StaticController < ApplicationController
     @allSectorViews = SecurityView.select("company_sectors.name, company_sectors.id, count(company_sectors.id) as views").where("security_views.created_at >= ?",Time.now-1.day).joins(:security, :company_sector).group("company_sectors.id").order("views desc").limit(5)
     @allIndustryViews = SecurityView.select("company_industries.name, company_industries.id, count(company_industries.id) as views").where("security_views.created_at >= ?",Time.now-1.day).joins(:security, :company_industry).group("company_industries.id").order("views desc").limit(5)
     @allFundCategoryViews = SecurityView.select("fund_categories.name, fund_categories.id, count(fund_categories.id) as views").where("security_views.created_at >= ?",Time.now-1.day).joins(:security, :fund_category).group("fund_categories.id").order("views desc").limit(5)
+    @lastTenSecurityViews = SecurityView.includes(:security).order("id desc").limit(10)
   end
 
   def about
